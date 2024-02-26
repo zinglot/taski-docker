3f8f3cc989b000dfa390d16abf380ca82d6d192a
Владимир Желтухов <zinglot772@gmail.com>
2024-02-26 23:51:30 +0500

    +

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,12 +1,14 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
